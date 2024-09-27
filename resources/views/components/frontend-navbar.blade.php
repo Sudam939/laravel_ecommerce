18a3f4411991bc31dd6051bfb6fe9ecd26878357
Sudam939 <sudamshrestha939@gmail.com>
2024-09-27 21:35:29 +0545

    user dashboard

--- resources/views/components/frontend-navbar.blade.php
+++ resources/views/components/frontend-navbar.blade.php
@@ -10,15 +10,21 @@
                     <div class="flex">
                         <input type="search" name="q" id="search" class="w-full rounded-s-lg"
                             placeholder="compare products">
-                        <button type="submit"
-                            class="bg-gray-300 text-[#009900] font-bold px-4 rounded-e-lg">COMPARE</button>
+                        <button type="submit" class="bg-gray-300 primary font-bold px-4 rounded-e-lg">COMPARE</button>
                     </div>
                 </form>
             </div>
 
-            <div>
-                <a href="" class="bg-[#2bbb2b] px-4 py-1 rounded-md text-white">SignUp</a>
-                <a href="" class="bg-[#ffcc33] px-4 py-1 rounded-md">Login <i class="fa-solid fa-arrow-right"></i></a>
+            <div class="flex gap-6 items-center">
+                @if (Auth::Check() && Auth::guard('web')->user())
+                    <a href="{{ route('register') }}" class="text-white text-xl"><i
+                            class="fa-solid fa-cart-shopping"></i></a>
+                    <a href="{{ route('dashboard') }}" class="secondary text-xl"><i class="fa-solid fa-user"></i></a>
+                @else
+                    <a href="{{ route('register') }}" class="border border-white px-4 py-1 rounded-md text-white">SignUp</a>
+                    <a href="{{ route('login') }}" class="bg-secondary px-4 py-1 rounded-md">Login <i
+                            class="fa-solid fa-arrow-right"></i></a>
+                @endif
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Frontend\PageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,3 +10,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::post('/vendor-store', [PageController::class, 'vendor_store'])->name('vendor_store');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
